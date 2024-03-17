6346ef8237078c6b950f69be0c0dfabdfa2c6a09
SungHo.You <hahaysh@hotmail.com>
2024-03-17 16:16:14 +0900

    update AAA

--- src/ApplicationCore/Services/BasketService.cs
+++ src/ApplicationCore/Services/BasketService.cs
@@ -23,7 +23,7 @@ public class BasketService : IBasketService
     {
         var basketSpec = new BasketWithItemsSpecification(username);
 
-        //PR 99999
+        //PR AAAA
         var basket = await _basketRepository.FirstOrDefaultAsync(basketSpec);
         //Basket basket = await _basketRepository.GetBySpecAsync(basketSpec);
 
